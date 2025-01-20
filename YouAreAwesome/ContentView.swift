fda683c56ae9748ebb18fedca6032f1c4e325131
John Eigenbrode <jeigenbrode@windstream.net>
2025-01-20 14:49:40 -0500

    Updates for types and arrays from Prof. Gallaughers SwiftUI Course 2025 Ch 1.8 and 1.9

--- YouAreAwesome/ContentView.swift
+++ YouAreAwesome/ContentView.swift
@@ -10,6 +10,8 @@ import SwiftUI
 struct ContentView: View {
     @State private var message = ""
     @State private var imageName = ""
+    @State private var imageNumber = 0
+    @State private var messageNumber = 0
     var body: some View {
         
         VStack {
@@ -26,15 +28,34 @@ struct ContentView: View {
                 .font(.largeTitle)
                 .fontWeight(.heavy)
                 .foregroundStyle(.red)
+                .multilineTextAlignment(.center)
             
             Spacer()
             
             HStack {
-                Button("Press Me!") {
-                    let message1 = "You are Awesome!"
-                    let message2 = "You are Great!"
-                    message = ( message == message1 ? message2 : message1)
-                    imageName = ( imageName == "image0" ? "image1" : "image0")
+                Button("Show Message!") {
+
+                    let messages = [
+                        "You are Awesome!",
+                        "You are Great!",
+                        "Fabulous? That's You!",
+                        "You are Magical!",
+                        "You are very Skilled!",
+                        "You have excelled to the highest level!",
+                        "When the Genius Bar Needs Help, They Call You!"
+                    ]
+
+                    message = messages[messageNumber]
+                    messageNumber += 1
+                    if messageNumber == messages.count {
+                        messageNumber = 0
+                    }
+                                
+                    imageName = "image\(imageNumber)"
+                    imageNumber += 1
+                    if imageNumber > 9 {
+                        imageNumber = 0
+                    }
                 }
                 .buttonStyle(.borderedProminent)
                 .font(.title2)
