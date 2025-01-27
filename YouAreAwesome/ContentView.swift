caff45c1d19d99954cc1a129bae9d31e41788c10
John Eigenbrode <jeigenbrode@windstream.net>
2025-01-27 16:11:38 -0500

    Added functions and refactoring for DRY  from Prof. G's Ch 1.16 2025

--- YouAreAwesome/ContentView.swift
+++ YouAreAwesome/ContentView.swift
@@ -57,49 +57,48 @@ struct ContentView: View {
                         
                     ]
                     
-                    var messageNumber : Int
-                    repeat {
-                        messageNumber = Int.random(in: 0..<messages.count-1)
-                    } while messageNumber == lastMessageNumber
-                    message = messages[messageNumber]
-                    lastMessageNumber = messageNumber
-                    
-                    
-                    var imageNumber : Int
-                    repeat {
-                        imageNumber = Int.random(in: 0...numberOfImages-1)
-                    } while imageNumber == lastImageNumber
-                    imageName = "image\(imageNumber)"
-                    lastImageNumber = imageNumber
-                    
-                    var soundNumber : Int
-                    repeat {
-                        soundNumber = Int.random(in: 0...numberOfSounds-1)
-                    } while soundNumber == lastSoundNumber
-                    imageName = "image\(soundNumber)"
-                    lastSoundNumber = soundNumber
-                    
-                    let soundName = "sound\(soundNumber)"
-                    guard let soundFile = NSDataAsset(name: soundName)?.data else {
-                        print(" 😡 Could not read file named \(soundName)")
-                        return }
-                    
-                    do{
-                        audioPlayer = try AVAudioPlayer(data: soundFile)
-                        audioPlayer?.play()
-                    } catch {
-                        print(" 😡 ERROR: \(error.localizedDescription) creating audioPlayer")
-                    }
-                    
+                    lastMessageNumber = nonRepeatingRandomInt(
+                        lastRandomNumber: lastMessageNumber,
+                        min: 0,
+                        max: messages.count-1
+                    )
+                    message = messages[lastMessageNumber]
+                 
+                    lastImageNumber = nonRepeatingRandomInt(lastRandomNumber: lastImageNumber, min: 0, max: numberOfImages-1)
+                    imageName = "image\(lastImageNumber)"
+                   
+                    lastSoundNumber = nonRepeatingRandomInt(lastRandomNumber: lastSoundNumber, min: 0, max: numberOfSounds-1)
+                    playSound(soundName: "sound\(lastSoundNumber)")
                 }
                 .buttonStyle(.borderedProminent)
                 .font(.title2)
             }
-            
         }
         .padding()
     }
-}
+    
+    func playSound(soundName: String) {
+        guard let soundFile = NSDataAsset(name: soundName)?.data else {
+            print(" 😡 Could not read file named \(soundName)")
+            return }
+        do{
+            audioPlayer = try AVAudioPlayer(data: soundFile)
+            audioPlayer?.play()
+        } catch {
+            print(" 😡 ERROR: \(error.localizedDescription) creating audioPlayer")
+        }
+    }
+    
+    func nonRepeatingRandomInt(lastRandomNumber: Int, min: Int, max: Int) -> Int {
+        var randomNumber = Int.random(in: min...max)
+        while randomNumber == lastRandomNumber {
+            randomNumber = Int.random(in: min...max)
+        }
+        return randomNumber
+    }
+        
+    }
+
 
 #Preview {
     ContentView()
